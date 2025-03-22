c9747be34c37546b6e2198f846e06cbe2029ca75
pypepe <pe.pe@centrum.sk>
2025-03-22 08:39:21 +0100

    Priebezny commit

--- school_app/urls.py
+++ school_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('hello-world', views.hello_world, name="hello-world"),
     path('subjects', views.list_subjects, name="list-subjects"),
+    path('subjects/<int:pk>',views.subject_detail, name="subject-detail"),
 ]
--- school_app/views.py
+++ school_app/views.py
@@ -8,7 +8,7 @@ from django.http import HttpResponse, JsonResponse, HttpResponseNotFound
 from django.views.decorators.csrf import csrf_exempt
 
 
-subjects = [{"name": "Maths"}, {"name": "PE"}]
+subjects = [{"id": 1, "name": "Maths"}, {"id": 2, "name": "PE"}]
 
 
 def hello_world(request):
@@ -34,3 +34,15 @@ def list_subjects(request):
         return JsonResponse(subject_dict, status=200)
     else:
         return HttpResponseNotFound("Sorry, this method is not supported")
+
+@csrf_exempt
+def subject_detail(request, pk):
+    global subjects
+
+    try:
+        subject = next(subject for subject in subjects if subject["id"] == pk)
+    except StopIteration:
+        return JsonResponse({"status": f"There is no subject with id {pk}"}, status=404)
+ 
+    if request.method == "GET":
+        return JsonResponse(subject)
