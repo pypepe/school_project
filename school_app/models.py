352e78796ce61ca96fdedaad9cbd40849f891830
pypepe <pe.pe@centrum.sk>
2025-04-01 08:59:35 +0200

    Declarations of models.py

--- school_app/models.py
+++ school_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=256)
+    abbrev = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"NAME: {self.name}, ABBREV: {self.abbrev}"
+    
