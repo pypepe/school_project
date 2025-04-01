5eee5b03fd9434306ba90cc170f3affe08bb9193
pypepe <pe.pe@centrum.sk>
2025-04-01 19:22:28 +0200

    Zmena DB prostredia na PostgreSQL

--- school_project/settings.py
+++ school_project/settings.py
@@ -74,12 +74,24 @@ WSGI_APPLICATION = 'school_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'school',
+        'USER': 'peter',
+        'PASSWORD': 'peter',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
